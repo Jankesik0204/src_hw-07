e3d8904e289a0f7642798596ff4c32762a79237a
Jankesik0204 <jankestmpl@wp.pl>
2024-01-03 17:29:06 +0100

    Dodaj pliki projektu

--- js/task-01.js
+++ js/task-01.js
@@ -13,8 +13,8 @@ const amount_of_LI2 = allCategories.length
 console.log('Number of categories: ', amount_of_LI2)
 
 for (const category of allCategories) {
-  console.log('Kategoria:', category.querySelector('h2').textContent)
+  console.log('Category:', category.querySelector('h2').textContent)
   const c1 = category.querySelector('ul') // Odnoścnik do znacznika <ul> w HTML
   const c2 = c1.querySelectorAll('li') // Odnośnik do znacznika <li> w HTML
-  console.log('Elemnets: ', c2)
+  console.log('Elemnets: ', c2.length)
 }
--- js/task-02.js
+++ js/task-02.js
@@ -8,18 +8,18 @@ const ingredients = [
 ]
 
 // 1.Utworzy oddzielny element <li>. Pamiętaj, aby użyć metody document.createElement().
-const a1 = document.querySelector('.categories')
-console.log('PRZED PENTLOM:', a1)
+const listElement = document.querySelector('#ingredients')
+// console.log('PRZED PENTLOM:', a1)
 
-for (const ingredient of ingredients) {
-  const newLI = document.createElement('li')
+ingredients.forEach((ingredient) => {
+  const li = document.createElement('li')
   console.log(ingredient)
   // 2.Doda nazwę składnika jako jego zawartość tekstową.
-  newLI.textContent = ingredient
+  li.textContent = ingredient
   // 3.Doda klasę item do elementu.
-  newLI.className = 'item'
+  li.className = 'item'
   // 4.Wstawi wszystkie <li> w jednej operacji do listy ul#ingredients.
-  a1.appendChild(newLI)
-}
+  listElement.appendChild(li)
+})
 
-console.log('PO PENTLI:', a1)
+// console.log('PO PENTLI:', a1)
